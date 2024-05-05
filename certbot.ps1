ca2dffa095c8900d3aa9df66aa7668dc85cd03ba
smwbalfe <smwbalfe@gmail.com>
2024-05-05 16:43:00 +0100

    first commit

---
+++ certbot.ps1
@@ -0,0 +1,1 @@
+docker run -it --rm -v K:\dev\shrillecho\shrillecho-app\nginx\etc3\letsencrypt:/etc/letsencrypt -v K:\dev\shrillecho\shrillecho-app\nginx\var3\lib2\letsencrypt:\var\lib\letsencrypt -p 80:80 -p 443:443 certbot/certbot certonly --standalone -d discord.shrillecho.app
---
+++ set_env.py
@@ -0,0 +1,39 @@
+import sys
+
+def update_environment_variables(env_file_paths, target_env):
+    for env_file_path in env_file_paths:
+        # Read the content of the .env file
+        with open(env_file_path, "r") as file:
+            lines = file.readlines()
+
+        # Update the ENVIRONMENT and NEXT_PUBLIC_ENV lines
+        with open(env_file_path, "w") as file:
+            for line in lines:
+                if line.startswith("ENVIRONMENT="):
+                    file.write(f'ENVIRONMENT="{target_env}"\n')
+                elif line.startswith("NEXT_PUBLIC_ENV="):
+                    file.write(f'NEXT_PUBLIC_ENV={target_env}\n')
+                else:
+                    file.write(line)
+
+if __name__ == "__main__":
+    # Check if an environment argument is provided
+    if len(sys.argv) != 2:
+        print("Usage: python update_env_vars.py <env>")
+        sys.exit(1)
+
+    # The environment to set ('dev' or 'prod')
+    target_env = sys.argv[1]
+
+    # Validate the environment argument
+    if target_env not in ["dev", "prod"]:
+        print("Error: <env> must be 'dev' or 'prod'")
+        sys.exit(1)
+
+    env_paths = [
+        r"K:\dev\shrillecho\shrillecho-app\web\.env.local",
+        r"K:\dev\shrillecho\shrillecho-app\server\.env",
+        r"K:\dev\shrillecho\shrillecho-discord\.env"
+    ]
+
+    update_environment_variables(env_paths, target_env)
